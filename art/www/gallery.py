719d7d0c4cfb6f2a9cb6068316b5a731f1ef6b33
Arun Joy Thekkiniyath <16219238+arunjoyt@users.noreply.github.com>
2024-05-17 10:36:02 +0530

    fixed caching problem that is happening in prodiction alone

--- art/www/gallery.py
+++ art/www/gallery.py
@@ -7,4 +7,7 @@ def get_context(context):
         doc = frappe.get_doc("ART Artifact", record.name).as_dict()
         art_display_items.append(doc)
     context.art_display_items = art_display_items
+    # if caching is enabled, new Aritfacts added won't be displayed on webpage unless cache is cleared.
+    # this happens only in prodoction, works fine in developemnt even when cache is enabled.
+    context.no_cache = 1
     return context
