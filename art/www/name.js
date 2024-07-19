4326b13f7bb9f49201131d028826d4f0c2c9aea1
arunjoyt <16219238+arunjoyt@users.noreply.github.com>
2024-07-19 17:14:44 +0530

    testing again

--- art/www/name.js
+++ art/www/name.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const name = "Wick";  // Replace with your actual name
+    const name = "Neo";  // Replace with your actual name
     const age = 25;            // Replace with your actual age
 
     document.getElementById('name').textContent = name;
