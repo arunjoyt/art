acc7d361e1dc655af1015ddf933f63c76d56599d
Arun Joy Thekkiniyath <arunjoyt@Aruns-MacBook-Air.local>
2024-07-19 16:58:01 +0530

    testing portal pages with sample websites, http://127.0.0.1:8000/portfolio/home, http://127.0.0.1:8000/name, http://127.0.0.1:8000/nameage/nameage

---
+++ art/www/name.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const name = "John";  // Replace with your actual name
+    const age = 25;            // Replace with your actual age
+
+    document.getElementById('name').textContent = name;
+    document.getElementById('age').textContent = `Age: ${age}`;
+});
---
+++ art/www/nameage/nameage.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const name = "Doe";  // Replace with your actual name
+    const age = 35;            // Replace with your actual age
+
+    document.getElementById('name').textContent = name;
+    document.getElementById('age').textContent = `Age: ${age}`;
+});
